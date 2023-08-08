4c0158e9cc20b190898ea316f79ff6d7b8982baf
CN171_11 <3428166361@qq.com>
2023-08-08 08:20:18 +0800

    Add files via upload

--- nonebot_plugin_mcversion/__init__.py
+++ nonebot_plugin_mcversion/__init__.py
@@ -3,9 +3,10 @@ from nonebot.plugin import PluginMetadata
 __plugin_meta__ = PluginMetadata(
     name="MC版本更新检测",
     description="一个用于检测MC最新版本的插件",
-    usage="使用mcver以获取最新版本号",
+    usage="使用 mcver 以获取最新版本号",
     type="application",
     homepage="https://github.com/CN171-1/nonebot_plugin_mcversion",
+    supported_adapters={"~onebot.v11"},
 )
 
 # 导入必要的库
